de5a990269b52b54fe9ab4b66afbf496ba212ee9
jermell <alex200399@gmail.com>
2016-10-23 07:47:51 -0500

    notebooks.

--- placemaker/src/placemaker/settings.py
+++ placemaker/src/placemaker/settings.py
@@ -3,6 +3,4 @@ MONGO_PORT = 27017
 MONGO_DB = 'placemaker-dev'
 NOT_CONFIGURED_MESSAGE = 'YOU NEED TO CONFIGURE twilio_account_sid or twilio_account_token or twilio_number'
 MIDDLEWARE_NOT_USED = 'configure this properly.'
-'''
-
-'''
+OPEN_WEATHER = '304bb5bb169f65dc73a1c17c5564afea'
